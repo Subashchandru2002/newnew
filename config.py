25ff2de5aa2033fd160a0c5423646ddcc08d2c74
Subashchandru2002 <subashwork40@gmail.com>
2025-12-19 00:58:37 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+# This should trigger: "Hardcoded identification information"
+DATABASE_PASSWORD = "Admin123!_Secret_Password"
+AWS_SECRET_KEY = "AKIAIMORKEEXAMPLES" 
+
+def connect():
+    print(f"Connecting with {DATABASE_PASSWORD}")
